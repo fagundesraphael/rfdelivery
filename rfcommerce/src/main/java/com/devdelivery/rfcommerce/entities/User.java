dac5cbf9e47febf8a62e0134c378a49fa9b1af66
Raphael Fagundes <fagundesraphael7@gmail.com>
2023-01-16 15:42:03 -0400

    Payment

--- rfcommerce/src/main/java/com/devdelivery/rfcommerce/entities/User.java
+++ rfcommerce/src/main/java/com/devdelivery/rfcommerce/entities/User.java
@@ -3,6 +3,9 @@ package com.devdelivery.rfcommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "tb_user")
 public class User {
@@ -15,6 +18,8 @@ public class User {
     private LocalDate birthDate;
     private String password;
 
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
     public User() {
     }
 
@@ -73,5 +78,9 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 }
