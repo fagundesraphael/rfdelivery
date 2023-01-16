9486027469efd6ade2881a1c236e516ba0c00407
Raphael Fagundes <fagundesraphael7@gmail.com>
2023-01-16 11:18:36 -0400

    first commit

---
+++ rfcommerce/src/main/java/com/devdelivery/rfcommerce/RfcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.devdelivery.rfcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfcommerceApplication.class, args);
+	}
+
+}
---
+++ rfcommerce/src/test/java/com/devdelivery/rfcommerce/RfcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdelivery.rfcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
